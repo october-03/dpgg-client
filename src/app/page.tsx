763871d9a3498d185a231f24a769435a792ac7a9
october-03 <october03@o365sen.net>
2023-02-03 23:55:24 +0900

    메인화면 로고

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import NavBar from "@/components/common/NavBar";
 import Input from "@/components/main/Input";
+import Image from "next/image";
 import "./style.css";
+import Icon from "../../public/images/purpleIcon.png";
 
 export default function Home() {
   return (
     <main>
       <div className="background" />
       <NavBar choose="홈" />
-      <div className="inputWrapper">
+      <div className="flex-center">
+        <Image src={Icon} alt="" style={{ width: 240, height: 197, marginBottom: 33 }} />
+      </div>
+      <div className="flex-center">
         <Input />
       </div>
     </main>
