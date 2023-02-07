76ea32571f420796e74afab9c75550c672fd8b69
october-03 <october03@o365sen.net>
2023-02-07 21:01:48 +0900

    env 설정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const Dotenv = require("dotenv-webpack");
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
